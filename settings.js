079601534598355bc05172f69b28f492d1e6fbb6
Chethmina Kavishan <atadeka999@gmail.com>
2024-09-20 09:23:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: 'QUEENELISA;;;==wYTRTTtgUYZVGNTVkMwElVyRVT1QUOk9lMyRUYwlFWCBFd4hESfNnWNFnNjwUUSVkUwUVOpwd=31001597749',
+  SESSION_ID: 'QUEENELISA;;;==waQtGeNlVMqx0SVNXRNFjVfBjbWZ1V4NTZWRmQOZWOqlGNYRESpllcrlDRjkkciJUVxkEOpwd=31001597749',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
